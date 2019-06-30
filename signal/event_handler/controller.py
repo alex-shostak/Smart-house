7d9db261a6079bd0d38f59c6fdb937111360a4a8
alex-shostak <shostak.alexandr92@gmail.com>
2019-06-30 23:09:29 +0300

    refactor

--- signal/event_handler/controller.py
+++ signal/event_handler/controller.py
@@ -1,6 +1,6 @@
 from helpers.http import Api
 import helpers.http_responses as http_responses
-import door_sensor.service as service
+import service as service
 from helpers.telegram import TelegramApi
 
 
@@ -13,16 +13,6 @@ def get_mode(request, response):
     response.text = service.get_mode()
 
 
-@Api.route('/set_mode')
-def set_mode(request, response):
-    # authenticate?
-    mode = request.GET.get('mode')
-    if mode is None:
-        http_responses.bad_request(response)
-        service.set_mode(mode)
-    http_responses.ok(response)
-
-
 @Api.route('/save_event')
 def save_event(request, response):
     event_name = request.GET.get('name')
@@ -31,11 +21,6 @@ def save_event(request, response):
     http_responses.ok(response)
     reply_markup = {"inline_keyboard": [[{ "text": "Я", "callback_data": "me" }],[{ "text": "Не я", "callback_data": "not_me" }]]}
     TelegramApi.send_message(token='752125381:AAHZtMmxpRbQKF8aTOE1aTyP1zAyX9XD-SY', chat_id='-1001375176524', text='Кто пришел домой?', reply_markup=reply_markup)
-
-
-@Api.route('/telegram_webhook')
-def telegarm_webhook(request, response):
-    print(request)
 
 
 
