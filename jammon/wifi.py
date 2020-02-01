ae4880cb8fc1ae20fc599f779bfb97a15951f515
alex-shostak <shostak.alexandr92@gmail.com>
2020-02-01 23:08:58 +0200

    switched to DHCP

--- jammon/wifi.py
+++ jammon/wifi.py
@@ -15,4 +15,5 @@ def connect():
         sta_if.connect(config.WiFi.SSID, config.WiFi.pwd)
         while not sta_if.isconnected():
             machine.idle()
-    sta_if.ifconfig((config.WiFi.ip, config.WiFi.mask, config.WiFi.gate, '8.8.8.8'))
+    #sta_if.ifconfig((config.WiFi.ip, config.WiFi.mask, config.WiFi.gate, '8.8.8.8'))
+    sta_if.ifconfig()
